9e8b3900b3f4776b6cf3f6003b182af3b214c9db
Kuroii Paakin <kuroiipaakin@Kuroiis-MacBook-Air.local>
2026-02-04 00:08:01 -0600

    react-TV-finish

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+import { useState } from "react";
+import showsData from "./shows/data";
+import ShowSelection from "./shows/ShowSelection";
+import ShowDetails from "./shows/ShowDetails";
 export default function App() {
+
+  const [shows] = useState(showsData);
+
+  const [selectedShow, setSelectedShow] = useState(null);
   return (
     <>
       <header>
         <p>React TV</p>
+
+          <ShowSelection
+          shows={shows}
+          setSelectedShow={setSelectedShow}
+        />
       </header>
-      <main></main>
+      <main>
+          <ShowDetails show={selectedShow} />
+      </main>
     </>
   );
 }
